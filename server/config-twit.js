ad3fee8a7bbf7359f77a74610862e80a30cc717b
Isaac Strack <strack@gmail.com>
2015-05-07 17:31:50 -0600

    twitter keys

--- server/config-twit.js
+++ server/config-twit.js
@@ -1,10 +1,10 @@
 Twitter = Meteor.npmRequire('twit');
 
 Twit = new Twitter({
-  consumer_key: 'egrdttx65TmHwFcFMxJ91343245BeMR8',
-  consumer_secret: 'fR2r02CGUnJCDtVUUIeMij223452345234WjNiqxtOo6QPqvzyZr3AIUnxztb',
-  access_token: '16226749-q84pox234543yAnEkn32354xMi9acUhIgC9I52345543EOApOI2Z3DvDZM',
-  access_token_secret: '7mrZel7Kr8j352445352342oT74534523tCyJ3zeQC8t542gdeddzpiDuaqtRaij914'
+  consumer_key: 'egrdttx65TmHwFcFMxJ9BeMR8',
+  consumer_secret: 'fR2r02CGUnJCDtVUUIeMij2WjNiqxtOo6QPqvzyZr3AIUnxztb',
+  access_token: '16226749-q84poxyAnEkn3xMi9acUhIgC9I5EOApOI2Z3DvDZM',
+  access_token_secret: '7mrZel7Kr8joT7tCyJ3zeQC8tgdeddzpiDuaqtRaij914'
 });
 
 simplifyTweet = function(tweet){
